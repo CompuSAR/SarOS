73ad1f0241b933e9c31201bcf49acd83c6b805b8
Shachar Shemesh <shachar@shemesh.biz>
2021-06-16 17:37:12 +0300

    Fix compilation errors

--- leds.cpp
+++ leds.cpp
@@ -1,8 +1,10 @@
-void ledsOn() {
-    static constexpr intptr baseAddress = 0x0300B000;
-    static constexpr intptr PC_CFG1 = baseAddress + 0x004c;
-    *reinterpret_cast<volatile int32_t *>(PC_CFG1) = 0x77117777;
+#include <stdint.h>
 
-    static constexpr intptr PC_DAT = baseAddress + 0x0058;
-    *reinterpret_cast<volatile int32_t *>(PC_DAT) = 0x3000;
+void ledsOn() {
+    static constexpr intptr_t baseAddress = 0x0300B000;
+    static constexpr intptr_t PC_CFG1 = baseAddress + 0x004c;
+    *reinterpret_cast<volatile uint32_t *>(PC_CFG1) = 0x77117777;
+
+    static constexpr intptr_t PC_DAT = baseAddress + 0x0058;
+    *reinterpret_cast<volatile uint32_t *>(PC_DAT) = 0x3000;
 }
