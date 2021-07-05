3e26d85feb10833b04a517674952b1c63b24efcc
Shachar Shemesh <shachar@shemesh.biz>
2021-07-05 13:41:52 +0300

    Code is running on Pi

--- leds.cpp
+++ leds.cpp
@@ -8,3 +8,11 @@ void ledsOn() {
     static constexpr intptr_t PC_DAT = baseAddress + 0x0058;
     *reinterpret_cast<volatile uint32_t *>(PC_DAT) = 0x3000;
 }
+
+extern "C"
+void reset() {
+    ledsOn();
+
+    while(true)
+        ;
+}
